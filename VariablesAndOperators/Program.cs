1261d49ab09414a87c9eedd382bd2b8e975ec628
kingveee <vivianpham22@gmail.com>
2017-06-27 15:11:15 -0400

    class excerise

--- VariablesAndOperators/Program.cs
+++ VariablesAndOperators/Program.cs
@@ -10,6 +10,44 @@ namespace VariablesAndOperators
     {
         static void Main(string[] args)
         {
+            
+
+           
+
+           
+
+
+
+            // Problem 2 
+            int completeCups = 7;
+            int cupsUsed = 2;
+        
+            int cupsNeeded = (completeCups - cupsUsed);
+            Console.WriteLine(cupsNeeded);
+           
+            //Problem 3
+            int moneyAvailable = 40;
+            int packageCost = 8;
+            int packagePurchased = (moneyAvailable / packageCost);
+            Console.WriteLine(packagePurchased);
+
+            //Donation Tracker
+            Console.WriteLine("Name of Donor");
+            string donorName = Console.ReadLine();
+
+            Console.WriteLine("Address of Donor");
+            string donorAddress = Console.ReadLine();
+
+            Console.WriteLine("Email of Donor");
+            string donorEmail = Console.ReadLine();
+
+            Console.WriteLine("Fiscal Year");
+            string year = Console.ReadLine();
+
+            
+
+
+
         }
     }
 }
